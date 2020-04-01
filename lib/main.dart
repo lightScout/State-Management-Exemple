b1c4e02f2b7288770bfe12372bf73217e3deb43c
lightScout <Music1:)>
2020-04-01 08:13:08 +0100

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Level1(),
+    );
+  }
+}
+
+class Level1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Level2(),
+    );
+  }
+}
+
+class Level2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Level3(),
+    );
+  }
+}
+
+class Level3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Needs Data'),
+    );
+  }
+}
