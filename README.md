49cd6b7e3bdad4cf650d9fdbd89bf1125b5d5f5e
lightScout <Music1:)>
2020-04-04 11:55:45 +0100

    add ChangeNotifierProvider add notifyListeners update README

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # State Management
 
-This project is exemplifying state management using the Provider package.
+Simple 3 layer project used to exemplify state management using the Provider package.
+
+Things to pay attention:
+
+*'ChangeNotifierProvider' on the root of the widget tree -
+which allows for the rest of the three to see data and react to changes coming from the Provider.
+
+*'notifyListeners()' use to send a flag to all listeners to rebuild.
+
+It is a simply project, easy enough to understand what is going on and to play around with.
+
+    More details through the code.
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,15 @@ import 'package:provider/provider.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String data = 'Data';
   @override
   Widget build(BuildContext context) {
-    return Provider<String>(
-      create: (context) => data,
+    //ChangeNotifierProvider used in order to be listen for data/changes on data
+    return ChangeNotifierProvider<Data>(
+      create: (context) => Data(),
       child: MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text(data),
+            title: MyText(),
           ),
           body: Level1(),
         ),
@@ -33,8 +33,11 @@ class Level1 extends StatelessWidget {
 class Level2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Level3(),
+    return Column(
+      children: <Widget>[
+        MyTextField(),
+        Level3(),
+      ],
     );
   }
 }
@@ -43,7 +46,41 @@ class Level3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(Provider.of(context)),
+      child: Text(Provider.of<Data>(context).data),
     );
+  }
+}
+
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //EG: of how to listen/receive data from the provider
+    return Text(Provider.of<Data>(context).data);
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //'myData' is set as a listener of the Provider
+    //and will rebuild itself when 'notifyListeners()' is called
+    final myData = Provider.of<Data>(context);
+    return TextField(
+      onChanged: (newText) {
+        myData.changeString(newText);
+      },
+    );
+  }
+}
+
+//Data class extending 'ChangeNotifier' -> which allows interaction with 'Provider'
+class Data extends ChangeNotifier {
+  String data = 'Some Data';
+  //Necessary function to set the variable value
+  //'notifyListeners()' is a must in order for Widgets listening be able to rebuild themselves
+  //with the variable's new value
+  void changeString(String newString) {
+    data = newString;
+    notifyListeners();
   }
 }
